414b99340e3b9f20b4ce47c4deff45d2895be6a7
OHMAE Ryosuke <ryo@mm2d.net>
2025-03-15 17:21:00 +0900

    🎉release 0.0.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ plugins {
 val applicationName = "AIOI-News"
 val versionMajor = 0
 val versionMinor = 0
-val versionPatch = 4
+val versionPatch = 5
 
 android {
     namespace = "net.mm2d.news.aioi"
