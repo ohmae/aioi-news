594cb25cebe5c58ecd84817f6fa621f58aea57bf
OHMAE Ryosuke <ryo@mm2d.net>
2026-07-04 16:30:47 +0900

    baseline-profile tagetSdk 37

--- baseline-profile/build.gradle.kts
+++ baseline-profile/build.gradle.kts
@@ -15,7 +15,7 @@ android {
 
     defaultConfig {
         minSdk = 28
-        targetSdk = 36
+        targetSdk = 37
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
